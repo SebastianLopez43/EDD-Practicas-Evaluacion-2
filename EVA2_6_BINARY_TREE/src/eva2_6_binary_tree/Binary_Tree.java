2a441e3151e8f8cba4cfad963a85fc62b7abb609
José Sebastian López Ibarra <99367521+SebastianLopez43@users.noreply.github.com>
2022-10-26 09:51:56 -0600

    Binary_Tree

--- EVA2_6_BINARY_TREE/src/eva2_6_binary_tree/Binary_Tree.java
+++ EVA2_6_BINARY_TREE/src/eva2_6_binary_tree/Binary_Tree.java
@@ -14,17 +14,61 @@ public class Binary_Tree {
     
     // Método para el usuario
     public void add(int value) {
-       recursiveAdd(value, root);
-    }
-    
-    // Método recursivo de add
-    // Requiere dos parámetros: valor y el nodo actual
-    private void recursiveAdd(int value, Nodo actualNode) {
-        if (actualNode.getLeft() == null) {
-            
-        } else if (actualNode.getRight() == null){
-            
+        if (root == null) {
+           root = new Nodo(value);
+        } else {
+            recursiveAdd(value, root);
         }
     }
     
+    // Método recursivo add
+    // Requiere dos parámetros: valor y el nodo actual
+    private void recursiveAdd(int value, Nodo actualNode) {
+        /*
+        Criterios a tomar en cuenta 
+        
+        - El nodo recibido sea null -> La lista está vacía
+        - Decidir si el valor va a la derecha o a la izquierda        
+        
+        Verificar si el lado (nodo), está vacío
+        */
+        
+        Nodo newNode = new Nodo(value);
+        
+        if (value < actualNode.getValue()) { // El valor va hacia la izquierda
+            if (actualNode.getLeft() == null) {
+                actualNode.setLeft(newNode);
+            } else {
+                recursiveAdd(value, actualNode.getLeft());
+            }
+        } else if (value > actualNode.getValue()) { // El valor va hacia la derecha
+            if (actualNode.getRight() == null) {
+                actualNode.setRight(newNode);
+            } else {
+                recursiveAdd(value, actualNode.getRight());
+            }
+        } else { // Si el valor es igual
+            System.out.println("Elemento repetido.");
+        }
+    }
+    
+    /* 
+    Imprimir los valores: In Order
+    Izquierda - valor - derecha
+    */
+    
+    // Método público
+    public void inOrder() {
+        recursiveInOrder(root);
+        System.out.println("");
+    }
+    
+    // Método privado recusivo
+    private void recursiveInOrder(Nodo actualNode) {
+        if (actualNode != null) {
+            recursiveInOrder(actualNode.getLeft());
+            System.out.print(actualNode.getValue() + " ");
+            recursiveInOrder(actualNode.getRight());
+        }
+    }
 }
