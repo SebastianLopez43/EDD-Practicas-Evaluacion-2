374efa24d2cb21b88f3b6c403cbfd74fdcd19ec6
José Sebastian López Ibarra <99367521+SebastianLopez43@users.noreply.github.com>
2022-10-26 09:50:27 -0600

    Práctica 6_Binary_Tree

--- EVA2_6_BINARY_TREE/src/eva2_6_binary_tree/EVA2_6_BINARY_TREE.java
+++ EVA2_6_BINARY_TREE/src/eva2_6_binary_tree/EVA2_6_BINARY_TREE.java
@@ -1,7 +1,7 @@
 package eva2_6_binary_tree;
 
 /**
- * @author José Sebastian López Ibarra
+ * @aUthor José Sebastian López Ibarra
  * Friday October 21 2022
  */
 
@@ -10,7 +10,20 @@ public class EVA2_6_BINARY_TREE {
     /**
      * @param args the command line arguments
      */
-    
-    public static void main(String[] args) {        
+    public static void main(String[] args) {
+        Binary_Tree binTree = new Binary_Tree();
+        
+        binTree.add(13);
+        binTree.add(10);
+        binTree.add(18);
+        binTree.add(2);
+        binTree.add(11);
+        binTree.add(17);
+        binTree.add(20);
+        binTree.add(16);                
+        binTree.add(16);
+        
+        // Imprimir el árbol In Order
+        binTree.inOrder();
     }    
 }
